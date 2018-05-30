2cdd792dcfdcd95114246b6910cca73184f1a25f
Alex Zador <alex.zador@gmail.com>
2018-05-30 09:49:22 +0300

    Поправил строку

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -10,7 +10,7 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HELLO WORLD");
+            Console.WriteLine("HELLO, WORLD!");
         }
     }
 }
